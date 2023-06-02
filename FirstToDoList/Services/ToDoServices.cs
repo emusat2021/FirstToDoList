b02c399cfd9f651f796a85fdc5ec308320ed8ff2
Eugenia Musat <eugenia.musat@nepa.com>
2023-06-02 08:40:55 +0200

    Think the flow of that like : user to db and db to user.

--- FirstToDoList/Services/ToDoServices.cs
+++ FirstToDoList/Services/ToDoServices.cs
@@ -3,15 +3,23 @@ namespace FirstToDoList.Services;
 
     public class ToDoServices
     {
-        private List<string> ToDoListToMemory {get; set; } = new ();
+        private List<string> ToDoListInDataBase {get; set; } = new ();
         public async Task GetListFromDB(ToDoListModel model)
         {
             //db to user
             //model.ToDoList := ToDoListToMemory;
+            //model.ToDoList <= ToDoListToMemory;
+            //"=" and "=="
+
+
+            model.ToDoList.Clear();
+            model.ToDoList.AddRange(ToDoListInDataBase);
         }
         public async Task Save(ToDoListModel toDoListModel)
         {
-            ToDoListToMemory.Equals(toDoListModel.ToDoList);
+            //save user's data to db
+            ToDoListInDataBase.Clear();
+            ToDoListInDataBase.AddRange(toDoListModel.ToDoList);
         }
         
         
