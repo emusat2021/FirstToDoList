a851c5f73fc804319765c837b91be1e1cfc9362b
Eugenia Musat <eugenia.musat@nepa.com>
2023-06-08 13:38:37 +0200

    Change OnInitialized to async

--- FirstToDoList/Model/ToDoListModel.cs
+++ FirstToDoList/Model/ToDoListModel.cs
@@ -5,3 +5,11 @@ public class ToDoListModel
     public string? Id { get; set; }
     public List<string> ToDoList { get; set; } = new ();
 }
+//
+//1  List<bool, string>
+// List = med all
+// List  = done, prio
+//2
+//entry is done aller inte
+//obj sting, bool
+// use bool flage if when entry.
